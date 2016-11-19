25e2ec446e533cf39056a5e0e894a89bb6478b6a
charlanalves <charlan.job@gmail.com>
2016-11-19 17:19:26 -0200

    string de conexao para multiplos ambientes

---
+++ common/models/SignupForm.php
@@ -0,0 +1,61 @@
+<?php
+namespace common\models;
+
+use yii\base\Model;
+use common\models\User;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+    public $username;
+    public $email;
+    public $password;
+    public $User;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+        
+        $this->User = new User();
+        $this->User->username = $this->username;
+        $this->User->email = $this->email;
+        $this->User->setPassword($this->password);
+        $this->User->generateAuthKey();
+        
+        return $this->User->save() ? $this->User : null;
+    }
+  
+  
+}
