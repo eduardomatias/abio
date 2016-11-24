ade0e1d9729b7bbcc7b32ad2289c10d1b42ce92a
eduardomatias <eduardomatias.1989@gmail.com>
2016-11-24 01:43:49 -0200

    cadastro de jornal + upload de cadernos

---
+++ frontend/controllers/CadernoEdicoesController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use frontend\controllers\SiteController;
+use frontend\models\Journal;
+use frontend\models\Journal_pages;
+use frontend\models\Journal_session;
+
+class CadernoEdicoesController extends SiteController
+{
+    
+    public $enableCsrfValidation;
+    private $id_journal = null;
+    private $empresa = 1;
+    private $id_usuario = 1;
+    private $tp_caderno = null;
+    private $dt_publicacao = null;
+    private $hash = 'sASda2e2sa';
+    private $file_name = '';
+    
+    /**
+     * @inheritdoc
+     */
+    public function actionIndex()
+    {   
+        return $this->render('index');
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function actionGridJournal()
+    {         	
+        Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
+        $headers = Yii::$app->response->headers;
+        $headers->add('Content-type', 'text/xml');
+
+        $xml = '';
+        
+        return $this->renderPartial('@app/views/default/xmlMask', array("xml" => $xml));
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function actionWinUploadCaderno()
+    {   
+        $this->enableCsrfValidation = false;
+        Yii::$app->session->set('id_journal', null);
+        return $this->render('win-upload-caderno');
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function actionProcessaCaderno()
+    {
+        
+        $post = Yii::$app->request;
+        $this->tp_caderno = $post->post('tp');
+        $this->dt_publicacao = $post->post('dt');
+        $this->file_name = $file = $post->post('file');
+        
+        if(!$this->tp_caderno || !$this->dt_publicacao || !$file){
+            echo $this->tp_caderno . $this->dt_publicacao . $file;
+            return false;
+        }
+        
+        $connecton = \Yii::$app->db;
+        $transaction = $connecton->beginTransaction();
+        $origem = '../../vendor/FileUpload/server/php/files/'.$file;
+
+        try {
+            
+            // registra no banco
+            $this->salvaRegistro();
+            
+            // move pdf
+            $destino = 'uploads/unprocessed/'.$this->file_name;
+            //$this->movePDF($origem,$destino);
+
+            // salva id do jornal na sessao
+            Yii::$app->session->set('id_journal', $this->id_journal);
+            
+            $transaction->commit();
+            
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            $this->excluiPDF($origem);
+            return ['message'=>'Journal Session ('.$this->id_journal.') - ' . $e];
+        }
+        
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    private function movePDF($origem, $destino)
+    {   
+     
+        if(!($this->verificaPath($destino))){
+            throw new Exception('Erro ao tentar criar o diretórios "'.$destino.'"');
+        }
+        
+        if(!rename($origem, $destino)){
+            throw new Exception('Erro ao tentar mover o PDF (' . $origem . ' para ' . $destino . ').');
+        }
+        
+        return true;
+    
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    private function excluiPDF($pdf)
+    {   
+        return @unlink($pdf);
+    }
+    
+    /**
+     * @inheritdoc
+     * se não existir cria
+     */
+    private function verificaPath($path)
+    {
+        $pastas = explode('/', $path);
+        unset($pastas[count($pastas)-1]); // remove o arquivo
+        
+        $dir = '';
+        foreach ($pastas as $pasta) {
+            $dir .= $pasta.'/';
+            if(!is_dir($dir)){
+                if(!mkdir($dir, 0755)){
+                    return false;
+                } else {
+                    chmod($dir, 0755);
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    private function salvaRegistro()
+    {
+        $session = Yii::$app->session;
+        
+        if(!$session->has('id_journal')){
+            // insert journal
+            $journal = new Journal();
+            $journal->id_user = $this->id_usuario;
+             //$journal->journal_number = null;
+            $journal->publish_date = $this->dt_publicacao;
+            $journal->upload_date = Date('Y-m-d H:i:s');
+            $journal->save();
+            $this->id_journal = $journal->id_journal;
+            
+        } else {
+            $this->id_journal = $session['id_journal'];
+
+        }
+        
+        // insert session
+        $journal_session = new Journal_session();
+        $journal_session->id_journal = $this->id_journal;
+        $journal_session->path = $this->empresa.'/'.Date('Y/m/');
+        $journal_session->id_session = $this->tp_caderno;
+        $journal_session->file_name = $this->file_name;
+        $journal_session->save();
+        
+        /*
+        $this->file_name = $journal_session->id_journal_session . '-' . $this->id_usuario . '-' . Date('w-d-m-Y') . $this->hash . '.pdf';
+        
+        $journal_session = Journal_session::findOne($journal_session->id_journal_session);
+        $journal_session->file_name = $this->file_name;
+        $journal_session->save();
+        */
+    }
+    
+    
+}
---
+++ frontend/views/caderno-edicoes/index.php
@@ -0,0 +1,97 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use frontend\assets\DhtmlxAsset;
+DhtmlxAsset::register($this);
+
+$this->title = 'Caderno de edições';
+
+?>
+
+<input type="button" value="upload do caderno" onclick="W.enviaCanerno()" />
+
+<script>
+
+    W = function(){};
+    W.uploadCaderno = {};
+    var gridJournal;
+    
+    W.uploadCaderno.init = function() {
+        WindowsDhtmlx = new dhtmlXWindows();
+        W.uploadCaderno.window = WindowsDhtmlx.createWindow("uploadCaderno", 0,0, 800, 525);
+        W.uploadCaderno.window.button('minmax1').hide();
+        W.uploadCaderno.window.button('park').hide();
+        W.uploadCaderno.window.denyResize();
+        W.uploadCaderno.window.center();
+        W.uploadCaderno.close();
+
+        W.uploadCaderno.window.attachEvent("onClose", function(win){
+            W.uploadCaderno.close();
+        });
+
+    };
+
+    W.uploadCaderno.close = function() {
+        W.uploadCaderno.window.hide();
+        W.uploadCaderno.window.setModal(false);
+    };
+
+    W.uploadCaderno.show = function() {
+        W.uploadCaderno.window.show();
+        W.uploadCaderno.window.setModal(true);
+    };
+    
+    W.processaPDF = function(data) {
+        console.log(data);
+        
+        file = data[0].fileName;
+        dt = data[0].dataJournal;
+        tp = data[0].value;
+        
+        url = 'index.php?r=caderno-edicoes/processa-caderno';
+        params = 'file='+file+'&dt='+dt+'&tp='+tp;
+
+        dhtmlxAjax.post(url, params, function (a){
+            if(a.xmlDoc.status === 200){
+                console.log(a.xmlDoc);
+                dhtmlx.alert ({
+                    text:"<?= Yii::t("app", "As alterações foram salvas") ?>", 
+                    callback: function(){
+                        W.uploadCaderno.close();
+                        gridJournal.recarregaGrid();
+                    }
+                });
+            }
+        });
+        
+    };
+    
+    W.enviaCanerno = function() {
+        W.uploadCaderno.window.setText('Enviar Cadernos');
+        W.uploadCaderno.window.attachURL('index.php?r=caderno-edicoes/win-upload-caderno');
+        W.uploadCaderno.show();
+    }
+    
+    document.addEventListener("DOMContentLoaded", function(event) {
+        
+        window.testeLayout = new dhtmlXLayoutObject("layoutObj", "1C");
+	window.testeLayout.cells("a").setText('Jornais cadastrados');
+        
+	gridJournal = window.testeLayout.cells("a").attachGrid();
+        gridJournal.setHeader("Data,Jornal,Usuário,Excluir");
+        gridJournal.setInitWidths("100,*,100,100");
+        gridJournal.setColAlign("center,left,left,center");
+        gridJournal.setColTypes("ro,ro,ro,img");
+        gridJournal.init();
+        gridJournal.recarregaGrid = function() {
+            //gridJournal.load('index.php?r=caderno-edicoes/grid-journal');
+        }
+        gridJournal.recarregaGrid();
+        
+        W.uploadCaderno.init();
+    });
+    
+</script>
+
+<div id="layoutObj" style="position: relative; top: 0px; left: 0px; right: 0px; width: 100%!important; height: 350px;"></div>
