2d635671ae0693f2ce7dec0bde08f05c77bad579
charlanalves <charlan.job@gmail.com>
2016-11-28 23:28:16 -0200

    tradução calendario bootstrap datepicker

--- vendor/FileUpload/index.php
+++ vendor/FileUpload/index.php
@@ -204,7 +204,7 @@
 
 <script>
 $(document).ready(function(){
-            $("#dataJournal").datepicker({autoclose: true, format: 'dd/mm/yyyy', language: "pt-BR"});
+            $("#dataJournal").datepicker({autoclose: true, format: 'dd/mm/yyyy', language: "pt-BR", todayHighlight: true});
             $("#dataJournal").datepicker('show');
             
           
