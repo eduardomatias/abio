e5fb6dc0043d1c8ba8c5782d134ed7615873ca11
charlanalves <charlan.job@gmail.com>
2016-11-30 20:22:30 -0200

    deleted_date = null query Journal api

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -63,6 +63,7 @@ class ApiController extends ActiveController
               ->join('JOIN', 'journal_session', 'journal_session.id_journal = journal.id_journal')
               ->join('JOIN', 'session', 'session.id_session = journal_session.id_session')
               ->where('journal.deleted_date is null')
+              ->andWhere('journal.processing_date')
               ->orderBy('session.name ASC')
               ->limit('7');
         
@@ -92,6 +93,7 @@ class ApiController extends ActiveController
               ->join('JOIN', 'session', 'session.id_session = journal_session.id_session')
               ->where(['journal.publish_date' => $date])  
               ->andWhere('journal.deleted_date is null')
+              ->andWhere('journal.processing_date')
               ->orderBy('session.name ASC')
               ->limit('7');
         
