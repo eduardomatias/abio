afd4d944bbce19057254906afc1e1716488a122e
charlanalves <charlan.job@gmail.com>
2016-12-03 18:49:20 -0200

    teste importacao no linux

--- frontend/controllers/ImportarEdicaoController.php
+++ frontend/controllers/ImportarEdicaoController.php
@@ -33,11 +33,11 @@ class ImportarEdicaoController extends Controller
      */
     public function actionProcessaPdf()
     {
-        var_dump(ini_get('disable_functions')); // not available if shell_exec disabled
-var_dump(ini_get('safe_mode')); // not available if true
              $totalPages = [];
              
              ob_clean();
+        var_dump(ini_get('disable_functions')); // not available if shell_exec disabled
+var_dump(ini_get('safe_mode')); // not available if true
             $commandGetTotalPages = "pdftotext /var/www/html/abio/frontend/web/uploads/processed/1/2016/12/empresarial.pdf - | grep -c $'\f'";
             
             var_dump('iniciando processo');
