a8d52ef33d0d9f899c9f89f19cbd50b65fb3b18f
charlanalves <charlan.job@gmail.com>
2016-12-03 18:30:00 -0200

    teste importacao no linux

--- frontend/controllers/ImportarEdicaoController.php
+++ frontend/controllers/ImportarEdicaoController.php
@@ -37,7 +37,7 @@ class ImportarEdicaoController extends Controller
              $totalPages = [];
              
              ob_clean();
-            $commandGetTotalPages = "sudo pdftotext /var/www/html/abio/frontend/web/uploads/processed/1/2016/12/empresarial.pdf - | grep -c $'\f'";
+            $commandGetTotalPages = "sudo ls -la";
             var_dump('iniciando processo');
             
             exec($commandGetTotalPages, $totalPages);
